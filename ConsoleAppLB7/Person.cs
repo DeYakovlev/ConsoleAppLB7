5588789645bd740bcddc81813e1066e169bd6523
Lklm <Lklm@DESKTOP-OGQ13G3>
2026-08-31 21:47:28 +0300

    Добавьте файлы проекта.

---
+++ ConsoleAppLB7/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppLB7
+{
+   
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        // обязательно создаем пустой конструктор для сирреализации 
+        public Person() { }   
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+    }
+}
---
+++ ConsoleAppLB7/Student.cs
@@ -0,0 +1,24 @@
+﻿using ConsoleAppLB7;
+using System;
+using System.Xml.Linq;
+
+namespace ConsoleAppLB7
+{
+    public class Student : Person
+    {
+        public string Group { get; set; }
+        public double AverageGrade { get; set; }
+
+        public Student() { }
+
+        public Student(string name, int age, string group, double averageGrade)
+            : base(name, age)
+        {
+            Group = group;
+            AverageGrade = averageGrade;
+        }
+
+        public override string ToString()
+            => $"{Name}, {Age} лет, группа {Group}, средний балл {AverageGrade}";
+    }
+}
