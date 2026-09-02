e94630914cb47c1111a9a700b829fff726e3af0a
Lklm <Lklm@DESKTOP-OGQ13G3>
2026-09-02 22:43:42 +0300

    update

--- ConsoleAppLB7/Program.cs
+++ ConsoleAppLB7/Program.cs
@@ -8,8 +8,51 @@ namespace ConsoleAppLB7
 {
     internal class Program
     {
+        public static void Gaf() 
+        {
+            Console.WriteLine("Гаф");
+        }
+      
         static void Main(string[] args)
         {
+             Action a = Gaf;
+             Student student = new Student();
+             Student student1 = new Student();
+             Student student2 = new Student();
+             Student student3 = new Student();
+
+            student.SetSpeak(a);
+            student1.SetSpeak(delegate () { Console.WriteLine("Я аноним");});
+            student2.SetSpeak(() => Console.WriteLine("А я вот лямбда"));
+            student3.SetSpeak(a);
+
+        
+
+            student.Speak();
+            student1.Speak();
+            student2.Speak();
+
+
+            //student3.SpeakComparison(student1);
+            //student3.SpeakComparison(student2);
+            Console.WriteLine(student3.SpeakComparison(student));
+            Console.WriteLine(student3.SpeakComparison(student2));
+
+            student.AddSpeak(() => Console.WriteLine("Мяу?"));
+
+            student.Speak();
+
+
+            student.SetGainWeight((x) => student.Weight += x);
+            student.GainWeight(30);
+            Console.WriteLine(student.Weight);
+
+            student2.Weight = 10;
+            student2.SetGainWeight((x) => student2.Weight *= x);
+            student2.GainWeight(30);
+            Console.WriteLine(student2.Weight);
+
+
         }
     }
 }
--- ConsoleAppLB7/Student.cs
+++ ConsoleAppLB7/Student.cs
@@ -6,8 +6,13 @@ namespace ConsoleAppLB7
 {
     public class Student : Person
     {
+        private Action _speak;
+
+        private Action<double> _gainWeight;
         public string Group { get; set; }
         public double AverageGrade { get; set; }
+
+        public double Weight { get; set; }
 
         public Student() { }
 
@@ -20,5 +25,37 @@ namespace ConsoleAppLB7
 
         public override string ToString()
             => $"{Name}, {Age} лет, группа {Group}, средний балл {AverageGrade}";
+
+        public void SetSpeak(Action action) 
+        {
+            _speak = action;
+            
+        }
+
+        public void SetGainWeight(Action<double> action) 
+        {
+            _gainWeight = action;
+
+        }
+
+        public void AddSpeak(Action action) 
+        {
+            _speak += action;
+        }
+
+        public void GainWeight(double amount) 
+        {
+            _gainWeight?.Invoke(amount);
+        }
+
+        public void Speak() 
+        {
+            _speak?.Invoke();
+        }
+
+        public bool SpeakComparison(Student student) 
+        {
+            return this._speak == student._speak;
+        }
     }
 }
